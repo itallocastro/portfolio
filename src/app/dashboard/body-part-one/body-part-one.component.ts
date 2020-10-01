a5f968c1e68c54647cfb4b3c5df78e8ad8fcb91a
itallocastro <itallopcastro@gmail.com>
2020-10-01 19:36:10 -0300

    mudança de cor e animações

--- src/app/dashboard/body-part-one/body-part-one.component.ts
+++ src/app/dashboard/body-part-one/body-part-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faRobot} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-body-part-one',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyPartOneComponent implements OnInit {
 
+  faRobot = faRobot;
   constructor() { }
 
   ngOnInit(): void {
